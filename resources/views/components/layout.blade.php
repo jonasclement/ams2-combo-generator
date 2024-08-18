f7776abc39755d402a0aa868f9c6908d0ce28fc7
Jonas Clément <contact@jonasclement.dk>
2024-08-18 18:55:36 +0200

    Add basic Blade view

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>{{ $title ?? config('app.name') }}</title>
+
+  <!-- CSS -->
+  @vite(['resources/sass/app.scss'])
+
+  <!-- Scripts -->
+  @vite(['resources/js/app.js'])
+</head>
+
+<body>
+  <div class="container">
+    <!-- TODO navigation on all pages? -->
+    {{ $slot }}
+  </div>
+</body>
+
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+  <div class="row">
+    <div class="col-12">Hello!</div>
+  </div>
+</x-layout>
